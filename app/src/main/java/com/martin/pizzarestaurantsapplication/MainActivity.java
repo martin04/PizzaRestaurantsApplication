7b8713f4b4da1bcc50df8caa6eb767099075f814
mr <mr@medialab.io>
2016-02-08 14:11:33 +0100

    colors and defining content provider in manifest

--- app/src/main/java/com/martin/pizzarestaurantsapplication/MainActivity.java
+++ app/src/main/java/com/martin/pizzarestaurantsapplication/MainActivity.java
@@ -1,13 +1,25 @@
 package com.martin.pizzarestaurantsapplication;
 
+import android.net.Uri;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RestaurantListFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RestaurantListFragment fragment = new RestaurantListFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment, RestaurantListFragment.TAG).commit();
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
 }
