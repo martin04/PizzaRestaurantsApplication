b9d4c5639f225d4448dabfef1f4b69aae711672c
mr <mr@medialab.io>
2016-02-08 11:55:56 +0100

    content provider created needs implementation

---
+++ app/src/main/java/com/martin/pizzarestaurantsapplication/contentprovider/RestaurantContentProvider.java
@@ -0,0 +1,11 @@
+package com.martin.pizzarestaurantsapplication.contentprovider;
+
+import android.content.ContentProvider;
+
+/**
+ * Created by medialab on 8.2.16.
+ */
+public class RestaurantContentProvider extends ContentProvider{
+
+    
+}
